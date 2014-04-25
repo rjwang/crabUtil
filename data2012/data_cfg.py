72d585eaa6447e42e93197fe56ade3147fb4954d
rjwang <r.jiewang@gmail.com>
2014-04-25 13:39:08 +0200

    update data

--- data2012/data_cfg.py
+++ data2012/data_cfg.py
@@ -3,6 +3,7 @@ isMC=False
 gtag="FT_53_V21_AN4::All"
 
 cfgFile=os.path.expandvars('${CMSSW_BASE}/src/CMGTools/HiggsAna2l2v/test/DijetAnalyzer_cfg.py')
+#cfgFile=os.path.expandvars('${CMSSW_BASE}/src/CMGTools/HiggsAna2l2v/test/2l2vDataAnalyzer_cfg.py')
 from CMGTools.HiggsAna2l2v.localPatTuples_cff import configureSourceFromCommandLine
 castorDir, outFile, inputList = configureSourceFromCommandLine()
 outFile='analysis.root'
