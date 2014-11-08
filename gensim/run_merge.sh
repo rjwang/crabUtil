5ccf699b5094a19e00a144831e4a08656c1a9af4
rjwang <r.jiewang@gmail.com>
2014-11-08 17:07:10 +0100

    update

--- gensim/run_merge.sh
+++ gensim/run_merge.sh
@@ -1,27 +1,27 @@
 #!bin/sh
-sh script_edmMerge_0.sh &
-sh script_edmMerge_1.sh &
-sh script_edmMerge_2.sh &
-sh script_edmMerge_3.sh &
-sh script_edmMerge_4.sh &
-sh script_edmMerge_5.sh &
-sh script_edmMerge_6.sh &
-sh script_edmMerge_7.sh &
-sh script_edmMerge_8.sh &
-sh script_edmMerge_9.sh &
-sh script_edmMerge_10.sh &
-sh script_edmMerge_11.sh &
-sh script_edmMerge_12.sh &
-sh script_edmMerge_13.sh &
-sh script_edmMerge_14.sh &
-sh script_edmMerge_15.sh &
-sh script_edmMerge_16.sh &
-sh script_edmMerge_17.sh &
-sh script_edmMerge_18.sh &
-sh script_edmMerge_19.sh &
-sh script_edmMerge_20.sh &
-sh script_edmMerge_21.sh &
-sh script_edmMerge_22.sh &
-sh script_edmMerge_23.sh &
-sh script_edmMerge_24.sh &
+sh script_edmMerge_0.sh >& script_edmMerge_0.log &
+sh script_edmMerge_1.sh >& script_edmMerge_1.log &
+sh script_edmMerge_2.sh >& script_edmMerge_2.log &
+sh script_edmMerge_3.sh >& script_edmMerge_3.log &
+sh script_edmMerge_4.sh >& script_edmMerge_4.log &
+sh script_edmMerge_5.sh >& script_edmMerge_5.log &
+sh script_edmMerge_6.sh >& script_edmMerge_6.log &
+sh script_edmMerge_7.sh >& script_edmMerge_7.log &
+sh script_edmMerge_8.sh >& script_edmMerge_8.log &
+sh script_edmMerge_9.sh >& script_edmMerge_9.log &
+sh script_edmMerge_10.sh >& script_edmMerge_10.log &
+sh script_edmMerge_11.sh >& script_edmMerge_11.log &
+sh script_edmMerge_12.sh >& script_edmMerge_12.log &
+sh script_edmMerge_13.sh >& script_edmMerge_13.log &
+sh script_edmMerge_14.sh >& script_edmMerge_14.log &
+sh script_edmMerge_15.sh >& script_edmMerge_15.log &
+sh script_edmMerge_16.sh >& script_edmMerge_16.log &
+sh script_edmMerge_17.sh >& script_edmMerge_17.log &
+sh script_edmMerge_18.sh >& script_edmMerge_18.log &
+sh script_edmMerge_19.sh >& script_edmMerge_19.log &
+sh script_edmMerge_20.sh >& script_edmMerge_20.log &
+sh script_edmMerge_21.sh >& script_edmMerge_21.log &
+sh script_edmMerge_22.sh >& script_edmMerge_22.log &
+sh script_edmMerge_23.sh >& script_edmMerge_23.log &
+sh script_edmMerge_24.sh >& script_edmMerge_24.log &
 
