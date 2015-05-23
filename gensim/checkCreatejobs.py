fd4c6eadfbe542533c3b843aba72c3f97b42a99a
rjwang <r.jiewang@gmail.com>
2015-05-24 01:56:19 +0200

    update checkCreatejobs.py

--- gensim/checkCreatejobs.py
+++ gensim/checkCreatejobs.py
@@ -15,8 +15,8 @@ with open(allsubmits) as fp:
 	    line1 = line.split(']')[0]
 	    setname = line1.split('[')[1]
 
-	    ALLSCRIPT.writelines('# multicrab -create -cfg '+ 'Multicrab_'+setname+'.cfg \n')
-	    ALLSCRIPT.writelines('# python ChangeXML.py -n 20 -c '+ setname+' \n\n')
+	    ALLSCRIPT.writelines(' multicrab -create -cfg '+ 'Multicrab_'+setname+'.cfg \n')
+	    ALLSCRIPT.writelines(' python ChangeXML.py -n 60 -c '+ setname+' \n\n')
 
 	    SCRIPT = open('Multicrab_'+setname+'.cfg',"w")
             SCRIPT.writelines('[MULTICRAB]\n')
@@ -28,11 +28,13 @@ with open(allsubmits) as fp:
 	    SCRIPT.writelines('USER.copy_data        = 0\n')
 	    #SCRIPT.writelines('\n')
             SCRIPT.writelines('USER.user_remote_dir  = /store/user/rewang/Jan21_2014/\n')
-	    SCRIPT.writelines('#submit_host      = cern_vocms83\n')
-	    SCRIPT.writelines('submit_host      = cern_vocms20\n')
-	    SCRIPT.writelines('#submit_host      = unl_hcc-crabserver\n')
-	    SCRIPT.writelines('#submit_host      = ucsd_submit-4\n')
-	    SCRIPT.writelines('#submit_host      = ucsd_submit-6\n')
+	    submithost = fp.next()
+	    SCRIPT.writelines(submithost)
+	    #SCRIPT.writelines('#submit_host      = cern_vocms83\n')
+	    #SCRIPT.writelines('submit_host      = cern_vocms20\n')
+	    #SCRIPT.writelines('#submit_host      = unl_hcc-crabserver\n')
+	    #SCRIPT.writelines('#submit_host      = ucsd_submit-4\n')
+	    #SCRIPT.writelines('#submit_host      = ucsd_submit-6\n')
 	    #if("[Data8TeV_" in line) : SCRIPT.writelines('CMSSW.lumi_mask       = Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt')
 	    SCRIPT.writelines('\n\n\n\n')
 
@@ -40,11 +42,12 @@ with open(allsubmits) as fp:
 	    lists.append(line)
 	    SCRIPT.writelines(line)
 	    nextline = fp.next()
+
 	    #print nextline
 	    SCRIPT.writelines('CMSSW.datasetpath               = None\n')
 	    SCRIPT.writelines('CMSSW.output_file               = Events.root\n')
-	    SCRIPT.writelines('CMSSW.total_number_of_events    = 50000\n')
-	    SCRIPT.writelines('CMSSW.events_per_job            = 20\n')
+	    SCRIPT.writelines('CMSSW.total_number_of_events    = 150000\n')
+	    SCRIPT.writelines('CMSSW.events_per_job            = 60\n')
 	    SCRIPT.writelines(nextline+'\n')
 
 	    SCRIPT.close()
